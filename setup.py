ebc3a75a6711dd1e61ef55bd160b248004242e55
Philip James <pjj@philipjohnjames.com>
2018-05-15 10:27:04 -0400

    We're not Python 2 compatible, let's stop lying about it.

--- setup.py
+++ setup.py
@@ -55,8 +55,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
